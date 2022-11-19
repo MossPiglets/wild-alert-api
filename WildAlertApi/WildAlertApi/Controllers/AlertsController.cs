61b4be672cfa58b421bec02e6eaf3c9bc5971cd3
Krysia <krystyna.szybalska@gmail.com>
2022-11-19 17:46:20 +0100

    CreatedAlert model and some other changes

--- WildAlertApi/WildAlertApi/Controllers/AlertsController.cs
+++ WildAlertApi/WildAlertApi/Controllers/AlertsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WildAlertApi.Models.Alerts;
 
 namespace WildAlertApi.Controllers;
 
@@ -14,9 +15,13 @@ public class AlertsController : ControllerBase
     }
 
     [HttpPost]
-    public IActionResult Post()
+    public IActionResult Post(CreateAlert createAlert)
     {
         // dodanie do bazy
+        // wstrzyknij applicationDBContext w konstruktorze tego kontrolera
+        // stwórz nową instancję klasy alert na podstawie createdAlert 
+        // dodaj nową instancję bazy używając dbcontextu
+        // zrób save changes i zwróc instancję alertu w ok
         return Ok();
     }
 }
---
+++ WildAlertApi/WildAlertApi/Models/Alerts/CreateAlert.cs
@@ -0,0 +1,9 @@
+﻿namespace WildAlertApi.Models.Alerts;
+
+public class CreateAlert
+{
+    public double Longitude { get; set; }    
+    public double Latitude { get; set; }
+    public string? Comments { get; set; }
+    public AnimalType Animal { get; set; }
+}
