207888ae32c27ea57b64835d2d5732da49a0c4c4
Morasiu <morasiu2@gmail.com>
2022-11-19 20:32:48 +0100

    string as enum

--- WildAlertApi/WildAlertApi/Program.cs
+++ WildAlertApi/WildAlertApi/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using WildAlertApi.Extensions;
 using WildAlertApi.Models;
@@ -6,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
