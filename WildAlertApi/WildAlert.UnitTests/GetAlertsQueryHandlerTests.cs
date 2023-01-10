5a91b315f1c1de8ad1ff0ffd7599da3d27634be9
Krysia <krystyna.szybalska@gmail.com>
2023-01-10 15:04:20 +0100

    Fixed the mistake

--- WildAlertApi/WildAlert.UnitTests/GetAlertsQueryHandlerTests.cs
+++ WildAlertApi/WildAlert.UnitTests/GetAlertsQueryHandlerTests.cs
@@ -25,18 +25,18 @@ public class GetAlertsQueryHandlerTests
         // Add alert examples
         var alerts = new List<AlertEntity>()
         {
-            new() {Latitude = 50.06169712305333, Longitude = 19.937262672461046, Comments = "the center"},
-            new() {Latitude = 50.07769712305333, Longitude = 19.937262672461046, Comments = "2 km north from the center"},
-            new() {Latitude = 50.06169712305333, Longitude = 19.977262672461046, Comments = "5 km west from the center"},
-            new() {Latitude = 49.90169712305333, Longitude = 19.937262672461046, Comments = "10km south from the center"},
-            new() {Latitude = 50.06169712305333, Longitude = 20.137262672461046, Comments = "25km east from the center"}
+            new() {Latitude = 50, Longitude = 20, Comments = "the center"},
+            new() {Latitude = 50+2*0.08, Longitude = 20, Comments = "2 km north from the center"},
+            new() {Latitude = 50, Longitude = 20-5*0.08, Comments = "5 km west from the center"},
+            new() {Latitude = 50-10*0.08, Longitude = 20, Comments = "10km south from the center"},
+            new() {Latitude = 50, Longitude = 20+25*0.08, Comments = "25km east from the center"}
         };
         _context.Alerts.AddRange(alerts);
         _context.SaveChanges();
     }
 
-    [TestCase(20.02, null)]
-    [TestCase(null, 50.05)]
+    [TestCase(20, null)]
+    [TestCase(null, 50)]
     [TestCase(null, null)]
     [Test]
     public async Task Handle_WhenNoLongitudeOrLatitudeGiven_ShouldReturnAllAlerts(double? longitude, double? latitude)
@@ -54,10 +54,10 @@ public class GetAlertsQueryHandlerTests
         result.Should().BeEquivalentTo(_context.Alerts);
     }
     
-    [TestCase(19.937262672461046, 50.06169712305333, 3)]
-    [TestCase(19.937262672461046, 50.06169712305333, 6)]
-    [TestCase(19.937262672461046, 50.06169712305333, 16)]
-    [TestCase(19.937262672461046, 50.06169712305333, 26)]
+    [TestCase(20, 50, 3)]
+    [TestCase(20, 50, 6)]
+    [TestCase(20, 50, 16)]
+    [TestCase(20, 50, 26)]
     [Test]
     public async Task Handle_WhenLongitudeAndLatitudeIsGiven_ShouldReturnAlertsInRadius(double longitude, double latitude, double radius)
     {
@@ -69,15 +69,13 @@ public class GetAlertsQueryHandlerTests
             Latitude = latitude,
             Radius = radius
         };
+        var radiusInDegrees = radius * 0.008;
         // Act
         var result = await sut.Handle(query, CancellationToken.None);
         // Assert
-        //TODO: fix assert
-        var radiusDegree = radius * 0.008;
-
         result.Should().OnlyContain(dto => 
-            dto.Latitude < latitude + radiusDegree && dto.Latitude > latitude - radiusDegree &&
-            dto.Longitude < latitude - radiusDegree && dto.Latitude > latitude + radiusDegree);
+            dto.Latitude < (latitude + radiusInDegrees) && dto.Latitude > (latitude - radiusInDegrees) &&
+            dto.Longitude < (longitude + radiusInDegrees) && dto.Longitude > (longitude - radiusInDegrees));
     }
 
 }
