3c6752095df39dfedd09a2b23668eda4c6da481d
Krysia <krystyna.szybalska@gmail.com>
2023-01-19 16:02:14 +0100

    Added unit tests for the sensor methods

---
+++ WildAlertApi/WildAlert.UnitTests/CreateSensorCommandHandlerTests.cs
@@ -0,0 +1,41 @@
+using FluentAssertions;
+using MapsterMapper;
+using WildAlert.Application.Requests.Sensors.Commands.CreateSensor;
+using WildAlert.Persistence;
+using WildAlert.UnitTests.Factories;
+
+namespace WildAlert.UnitTests;
+
+public class CreateSensorCommandHandlerTests
+{
+    private ApplicationDbContext _context = null!;
+    private IMapper _mapper = null!;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _context = ApplicationDbContextFactory.Create();
+        _mapper = new Mapper();
+    }
+
+    [Test]
+    public async Task Handle_ShouldCreateNewAlert()
+    {
+        // Arrange
+        var sut = new CreateSensorCommandHandler(_context, _mapper);
+        var command = new CreateSensorCommand
+        {
+            Longitude = 20,
+            Latitude = 30,
+            Name = "test sensor",
+        };
+        // Act
+        var result = await sut.Handle(command, CancellationToken.None);
+        // Assert
+        _context.Alerts.Should().NotBeEmpty();
+        result.Longitude.Should().Be(command.Longitude);
+        result.Latitude.Should().Be(command.Latitude);
+        result.Name.Should().Be(command.Name);
+        result.Id.Should().NotBeEmpty();
+    }
+}
